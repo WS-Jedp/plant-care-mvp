d9b18c707ad37409f95847bad489fc19a9ce7a23
Juan Esteban <jedp082@gmail.com>
2021-05-28 15:07:24 -0500

    Seed created 🌿

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Person;
 use App\Models\TypeUser;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -19,14 +20,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // $type_user = new TypeUser();
-        // $type_user->type = 'admin';
-        // DB::insert('insert into people  (name, direction, country, city, birthdate) values (?, ?, ?, ? ,?)', ['juanes', 'clle 42', 'Col', 'Medellin', new Date('05/02/2001')]);
-        // $user = new User();
-        // $user->email = 'jedp082@gmail.com';
-        // $user->password = new Hash('password123');
-        // $user->type_user = 1;
-        // $user->person_id = 1;
+        $person = new Person();
+        $person->name = 'Juanes';
+        $person->direction = 'Calle 42';
+        $person->country = 'Col';
+        $person->city = 'Medellin';
+        $person->save();
+
+        $type_user = new TypeUser();
+        $type_user->type = 'admin';
+        $type_user->save();
+
+        $user = new User();
+        $user->email = 'jedp082@gmail.com';
+        $user->password = Hash::make('password123');
+        $user->type_user_id = 1;
+        $user->person_id = 1;
+        $user->save();
 
         // \App\Models\User::factory(10)->create();
     }
